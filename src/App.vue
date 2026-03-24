fc8ba127619df45b2bd345c878971bd7c5a32ced
phanvantien2907 <vantienp530@gmail.com>
2026-03-24 20:42:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Tien from './components/Tien.vue';
+</script>
+
+<template>
+  <HelloWorld />
+  <Tien/>
+</template>
---
+++ src/components/Tien.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p class="text-orange-500 font-bold cursor-pointer">My name is {{ myName }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const myName: string = 'Tien';
+</script>
